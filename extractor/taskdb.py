31030bfc84ecbe82494f215c96a2306865a4eef9
Robert Stojnic <robert.stojnic@gmail.com>
2018-12-19 17:21:13 +0000

    Add exported NLP-progress and EFF data, and also all the arxiv papers we have indexed before. Add a parser for the database, so we can now load in the data and start evaluating our algorithms.

---
+++ extractor/taskdb.py
@@ -0,0 +1,161 @@
+import json
+from typing import Dict, Tuple, List
+
+class Link:
+    def __init__(self, d:Dict):
+        """
+        Make a new Link to a URL from the dictionary of values
+
+        :param d: a dictionary with the data (from the JSON)
+        """
+
+        self.title = ""
+        self.url = ""
+
+        if "title" in d:
+            self.title = d["title"]
+
+        if "url" in d:
+            self.url = d["url"]
+
+class SotaRow:
+    def __init__(self, d: Dict):
+        """
+        A row of the SOTA table
+
+        :param d: a dictionary with the data (from the JSON)
+        """
+
+        self.model_name = d["model_name"]
+        self.paper_title = ""
+        self.paper_url = ""
+        self.paper_date = None
+
+        if "paper_title" in d:
+            self.paper_title = d["paper_title"]
+
+        if "paper_url" in d:
+            self.paper_url = d["paper_url"]
+
+        if "paper_date" in d:
+            self.paper_date = d["paper_date"]
+
+        self.code_links = []
+        self.model_links = []
+
+        if "code_links" in d:
+            for code_d in d["code_links"]:
+                if code_d:
+                    self.code_links.append(Link(code_d))
+
+        if "model_links" in d:
+            for model_d in d["model_links"]:
+                if model_d:
+                    self.model_links.append(Link(model_d))
+
+        self.metrics = d["metrics"]
+
+
+class Dataset:
+    def __init__(self, d:Dict, parent=None):
+        """
+        Make a new Dataset instance from a dictionary of values
+
+        :param d: a dictionary with the data (from the JSON)
+        :param parent: The parent dataset if present, i.e if the dataset is a subdataset
+        """
+        if "subdataset" in d:
+            self.dataset = d["subdataset"]
+        else:
+            self.dataset = d["dataset"]
+        self.description = ""
+        if "description" in d:
+            self.description = d["description"]
+
+        self.parent = parent
+
+        self.sota_metrics = []
+        self.sota_rows = []
+
+        if "sota" in d:
+            if "metrics" in d["sota"]:
+                self.sota_metrics = d["sota"]["metrics"]
+            if "rows" in d["sota"]:
+                for sota_row_d in d["sota"]["rows"]:
+                    if sota_row_d:
+                        self.sota_rows.append(SotaRow(sota_row_d))
+
+        self.subdatasets = []
+        if "subdatasets" in d:
+            for subd_d in d["subdatasets"]:
+                if subd_d:
+                    self.subdatasets.append(Dataset(subd_d, parent=self))
+
+        self.dataset_links = []
+        if "dataset_links" in d:
+            for link_d in d["dataset_links"]:
+                if link_d:
+                    self.dataset_links.append(Link(link_d))
+
+        self.dataset_citations = []
+        if "dataset_citations" in d:
+            for link_d in d["dataset_citations"]:
+                if link_d:
+                    self.dataset_links.append(Link(link_d))
+
+
+class Task:
+    def __init__(self, d:Dict, parent=None):
+        """
+        Make a new Task from a dictionary of values
+
+        :param d: a dictionary with all the data (from the JSON)
+        :param parent: The parent Task (if any)
+        """
+        self.task = d["task"]
+        self.description = ""
+        if "description" in d:
+            self.description = d["description"]
+        self.parent = parent
+
+        self.categories = []
+        if "categories" in d:
+            self.categories = d["categories"]
+
+        self.datasets = []
+        if "datasets" in d:
+            for dataset_d in d["datasets"]:
+                if dataset_d:
+                    self.datasets.append(Dataset(dataset_d))
+
+        self.subtasks = []
+        if "subtasks" in d:
+            for subt_d in d["subtasks"]:
+                if subt_d:
+                    self.subtasks.append(Task(subt_d, parent=self))
+
+
+class TaskDb:
+    tasks = {}
+
+    @staticmethod
+    def get_task(name:str):
+        return TaskDb.tasks[name]
+
+    @staticmethod
+    def add_task(name:str, task:Task):
+        TaskDb.tasks[name] = task
+
+    @staticmethod
+    def load_tasks(json_files: List[str]):
+        """
+        Load tasks from a list of paths to JSON Files
+
+        :param json_files:
+        :return:
+        """
+        for json_file in json_files:
+            with open(json_file, "r") as f:
+                task_list = json.load(f)
+                for t in task_list:
+                    TaskDb.add_task(t["task"], Task(t))
