38dcc1c58ef34f6475ced98a0ae808a6c3c31172
Robert Stojnic <robert.stojnic@gmail.com>
2019-09-13 19:54:02 +0100

    Apparently the library only works with this one specific version of marshmallow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sota-extractor",
-    version="0.0.8",
+    version="0.0.9",
     description="Automatic SOTA (state-of-the-art) extraction.",
     long_description=io.open("README.md", "r").read(),
     long_description_content_type="text/markdown",
---
+++ sota_extractor/tests/test_load_save.py
@@ -0,0 +1,6 @@
+from sota_extractor.taskdb.v01 import taskdb
+
+def test_load_save():
+    tdb = taskdb.TaskDB()
+    tdb.load_tasks(["data/tasks/nlpprogress.json"])
+    tdb.export_to_file("test.json")
